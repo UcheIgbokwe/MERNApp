1cc4ce32766303f32fe25e819c3077fd36e36790
UcheIgbokwe <uchechukwu.igbokwe@zenithbank.com>
2020-08-18 07:17:17 +0100

    Adding a Modal and Custom button

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './users/pages/Users';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 
@@ -15,6 +16,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact={true}>
             <Users/>
+          </Route>
+          <Route path="/:userId/places" exact={true}>
+            <UserPlaces/>
           </Route>
           <Redirect to="/" />
         </Switch>
