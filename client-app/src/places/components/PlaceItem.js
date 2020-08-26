553c8e25e498dc03c98a2f0823723d3218e7e90c
UcheIgbokwe <uchechukwu.igbokwe@zenithbank.com>
2020-08-26 16:52:31 +0100

    Add context

--- client-app/src/places/components/PlaceItem.js
+++ client-app/src/places/components/PlaceItem.js
@@ -1,16 +1,31 @@
-import React,{ useState } from 'react'
+import React,{ useState, useContext } from 'react'
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
+import { AuthContext } from '../../shared/context/auth-context';
 
 
 const PlaceItem = props => {
+    const auth = useContext(AuthContext);
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
     const openMapHandler = () => setShowMap(true);
     const closeMapHandler = () => setShowMap(false);
+
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true);
+    }
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    }
+
+    const confirmDeleteHandler = () => {
+        console.log('DELETING...')
+    }
 
     return (
         <React.Fragment>
@@ -26,6 +41,21 @@ const PlaceItem = props => {
                     <h2>THE MAP!</h2>
                 </div>
             </Modal>
+            <Modal 
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header="Are you sure?" 
+                footerClass="place-item__modal-actions" 
+                footer={
+                <React.Fragment>
+                    <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+                    <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                </React.Fragment>}
+            >
+                <p>
+                    Do you want to proceed and delete this place? Please note that it can't be undone thereafter.
+                </p>
+            </Modal>
         
             <li className="place-item">
             <Card className="place-item__content">
@@ -39,8 +69,12 @@ const PlaceItem = props => {
                 </div>
                 <div className="place-item__actions">
                 <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                <Button to={`/places/${props.id}`}>EDIT</Button>
-                <Button danger>DELETE</Button>
+                {auth.isLoggedIn && 
+                    <React.Fragment>
+                        <Button to={`/places/${props.id}`}>EDIT</Button>
+                        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+                    </React.Fragment>
+                }
                 </div>
             </Card>
             </li>
---
+++ client-app/src/users/pages/Auth.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from 'react';
+
+import './Auth.css';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH, VALIDATOR_EMAIL } from '../../shared/util/validator';
+import { useForm } from '../../shared/hooks/form-hook';
+import Card from '../../shared/components/UIElements/Card';
+import { AuthContext } from '../../shared/context/auth-context';
+
+const Auth = () => {
+    const auth = useContext(AuthContext);
+    const [isLoginMode, setIsLoginMode] = useState(true);
+    const [formState, inputHandler, setFormData] = useForm(
+        {
+            email: {
+                value: '',
+                isValid: false
+            },
+            password: {
+                value: '',
+                isValid: false
+            }
+        }, false
+    );
+
+    //prevMode holds the previous mode in state and we can switch it.
+    const switchModeHandler = () => {
+        //set form data for the extra field provided depending on the mode selected.
+        if (!isLoginMode) {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: undefined
+                },
+                formState.inputs.email.isValid && formState.inputs.password.isValid
+            );
+        } else {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: {
+                        value: '',
+                        isValid: false
+                    }
+                },
+                false
+            );
+        }
+        setIsLoginMode(prevMode => !prevMode);
+    };
+
+    const authSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs)
+        auth.login();
+    }
+
+    return(
+        <Card className="authentication">
+            <h2>Login Required</h2>
+            <hr/>
+            <form onSubmit={authSubmitHandler}> 
+                {!isLoginMode && <Input id="name" element="input" type="text" label="Your Name" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a name" onInput={inputHandler}/>}
+                <Input id="email" element="input" type="email" label="Email" validators={[VALIDATOR_EMAIL()]} errorText="Please enter a valid email" onInput={inputHandler}/>
+                <Input id="password" element="input" type="password" label="Password" validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid password." onInput={inputHandler}/>
+        
+                <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SIGNUP'}</Button>    
+            </form>
+            <Button inverse onClick={switchModeHandler}>SWITCH TO{isLoginMode ? ' SIGNUP' : ' LOGIN'}</Button>
+        </Card> 
+    ); 
+};
+
+export default Auth;
